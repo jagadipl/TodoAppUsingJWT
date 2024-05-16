f91c41a4571e1ce0e4e4df3b938a621728d61363
Jagadip Lamsal <jagadipl@yahoo.com>
2024-05-15 21:19:05 -0400

    Todo With JWT

---
+++ taskapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Todo
+from .serializers import TodoSerializer
+
+# Create your views here.
+def homepage(request):
+    return render(request,'taskapp/index.html')
+
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TodoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
